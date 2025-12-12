69afad33663e5da13abd2668771ffa8d7e443b26
Samuel Ruiz <samuel@ruizsamuel.es>
2025-12-12 15:03:40 +0100

    Ajuste colores

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,29 +1,69 @@
 return {
-  'morhetz/gruvbox',
+  'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
   config = function()
-    -- Configurar antes de cargar el tema
-    vim.g.gruvbox_transparent_bg = 1
-    vim.g.gruvbox_contrast_dark = 'hard'
+    require('tokyonight').setup({
+      style = 'night',
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { bold = true },
+        functions = { bold = true },
+      },
+    })
     
-    vim.cmd('colorscheme gruvbox')
+    vim.cmd('colorscheme tokyonight')
     vim.cmd('set cursorline')
-    vim.cmd('set termguicolors!')  -- Desactivar truecolor
+    vim.cmd('set termguicolors!')  -- Usar 16 colores
     
-    -- Usar colores del terminal (0-15)
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+    -- Estilo Tokyo Night con 16 colores
+    vim.api.nvim_set_hl(0, 'Normal', { ctermfg = 15, bg = 'NONE', ctermbg = 'NONE' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
     vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE', ctermbg = 'NONE' })
     
-    vim.api.nvim_set_hl(0, 'CursorLineNr', { ctermfg = 11, bold = true })
-    vim.api.nvim_set_hl(0, 'Whitespace', { ctermfg = 9 })
+    -- Keywords y control flow (púrpura)
+    vim.api.nvim_set_hl(0, '@keyword', { ctermfg = 13, bold = true })
+    vim.api.nvim_set_hl(0, '@keyword.function', { ctermfg = 13, bold = true })
+    vim.api.nvim_set_hl(0, '@keyword.return', { ctermfg = 13, bold = true })
+    vim.api.nvim_set_hl(0, '@keyword.import', { ctermfg = 5, bold = true })
+    
+    -- Funciones (azul brillante)
+    vim.api.nvim_set_hl(0, '@function', { ctermfg = 12, bold = true })
+    vim.api.nvim_set_hl(0, '@function.call', { ctermfg = 12 })
+    vim.api.nvim_set_hl(0, '@method', { ctermfg = 12 })
+    
+    -- Types (cyan brillante)
+    vim.api.nvim_set_hl(0, '@type', { ctermfg = 14 })
+    vim.api.nvim_set_hl(0, '@type.builtin', { ctermfg = 14, bold = true })
+    
+    -- Strings (verde brillante)
+    vim.api.nvim_set_hl(0, '@string', { ctermfg = 10 })
+    
+    -- Numbers y constants (amarillo/naranja)
+    vim.api.nvim_set_hl(0, '@number', { ctermfg = 11 })
+    vim.api.nvim_set_hl(0, '@constant', { ctermfg = 11 })
+    
+    -- Properties y parameters (cyan)
+    vim.api.nvim_set_hl(0, '@property', { ctermfg = 6 })
+    vim.api.nvim_set_hl(0, '@parameter', { ctermfg = 6, italic = true })
+    
+    -- Variables (blanco)
+    vim.api.nvim_set_hl(0, '@variable', { ctermfg = 7 })
+    vim.api.nvim_set_hl(0, '@variable.builtin', { ctermfg = 9 })
+    
+    -- Comentarios (gris)
+    vim.api.nvim_set_hl(0, 'Comment', { ctermfg = 8, italic = true })
+    
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { ctermfg = 12, bold = true })
+    vim.api.nvim_set_hl(0, 'Whitespace', { ctermfg = 8 })
 
     -- Para Neotree con fondo transparente
     vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", ctermbg = "NONE" })
     vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
     vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "NONE", ctermbg = "NONE" })
-    vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { ctermfg = 10 })
+    vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { ctermfg = 12 })
 
   end,
 }
