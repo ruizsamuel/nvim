0cd7b4a61eb5df0dd6b079c7213510a771103a6b
Samuel Ruiz <samuel@ruizsamuel.es>
2025-12-12 14:58:51 +0100

    Gruvox

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,41 +1,29 @@
 return {
-  'catppuccin/nvim',
+  'morhetz/gruvbox',
   lazy = false,
   priority = 1000,
-  name = 'catppuccin',
   config = function()
-    require('catppuccin').setup({
-      flavour = 'mocha', -- latte, frappe, macchiato, mocha
-      transparent_background = true,
-      integrations = {
-        neotree = true,
-        treesitter = true,
-        telescope = true,
-        lsp_saga = true,
-        cmp = true,
-        buffer_line = true,
-      },
-    })
-
-    vim.cmd('colorscheme catppuccin')
+    -- Configurar antes de cargar el tema
+    vim.g.gruvbox_transparent_bg = 1
+    vim.g.gruvbox_contrast_dark = 'hard'
+    
+    vim.cmd('colorscheme gruvbox')
     vim.cmd('set cursorline')
-
-    -- Resaltado mejorado para keywords
-    vim.api.nvim_set_hl(0, '@keyword', { fg = "#CBA6F7", bold = true })
-    vim.api.nvim_set_hl(0, '@keyword.function', { fg = "#CBA6F7", bold = true })
-    vim.api.nvim_set_hl(0, '@keyword.return', { fg = "#CBA6F7", bold = true })
-    vim.api.nvim_set_hl(0, '@type', { fg = "#F5C2E7" })
-    vim.api.nvim_set_hl(0, '@function', { fg = "#89B4FA" })
-    vim.api.nvim_set_hl(0, '@variable', { fg = "#CDD6F4" })
-
-    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = "#A6E3A1" })
-    vim.cmd [[highlight Whitespace guifg=#F38BA8]]
+    vim.cmd('set termguicolors!')  -- Desactivar truecolor
+    
+    -- Usar colores del terminal (0-15)
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE', ctermbg = 'NONE' })
+    
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { ctermfg = 11, bold = true })
+    vim.api.nvim_set_hl(0, 'Whitespace', { ctermfg = 9 })
 
     -- Para Neotree con fondo transparente
-    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { fg = "#A6E3A1" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { ctermfg = 10 })
 
   end,
 }
