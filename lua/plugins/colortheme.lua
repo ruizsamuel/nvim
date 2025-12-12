66a79814e85cb4b969630b5cfc2e89422fc6f766
Samuel Ruiz <samuel@ruizsamuel.es>
2025-12-12 14:48:17 +0100

    Change colorscheme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,20 +1,33 @@
 return {
-  'tinted-theming/tinted-vim',
+  'catppuccin/nvim',
   lazy = false,
   priority = 1000,
+  name = 'catppuccin',
   config = function()
+    require('catppuccin').setup({
+      flavour = 'mocha', -- latte, frappe, macchiato, mocha
+      transparent_background = true,
+      integrations = {
+        neotree = true,
+        treesitter = true,
+        telescope = true,
+        lsp_saga = true,
+        cmp = true,
+        buffer_line = true,
+      },
+    })
 
-    vim.cmd('colorscheme base16-ayu-dark')
+    vim.cmd('colorscheme catppuccin')
     vim.cmd('set cursorline')
 
-    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = "#FFCC00" })
-    vim.cmd [[highlight Whitespace guifg=#FF0000]]
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = "#A6E3A1" })
+    vim.cmd [[highlight Whitespace guifg=#F38BA8]]
 
-    -- Para Neotree
+    -- Para Neotree con fondo transparente
     vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
-    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" }) -- si hay buffer no activo
-    vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "#000000" })
-    vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "#000000", fg = "#000000" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { fg = "#A6E3A1" })
 
   end,
 }
